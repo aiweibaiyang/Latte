d598f3676af1fdae62ec2142663da9e58986aef9
Tony <tony@gmail.com>
2020-03-01 22:14:28 +0800

    基础框架可用性验证

--- example/src/main/java/com/example/festec/MainActivity.java
+++ example/src/main/java/com/example/festec/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.festec;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import com.example.latte.app.Latte;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toast.makeText(Latte.getApplication(),"传入Context啦",Toast.LENGTH_LONG).show();
     }
 }
--- latte-core/src/main/java/com/example/latte/app/Configurator.java
+++ latte-core/src/main/java/com/example/latte/app/Configurator.java
@@ -4,6 +4,7 @@ import com.joanzapata.iconify.IconFontDescriptor;
 import com.joanzapata.iconify.Iconify;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.WeakHashMap;
 
 /**
@@ -11,7 +12,7 @@ import java.util.WeakHashMap;
  */
 
 public class Configurator {
-    private static final WeakHashMap<String,Object> LATTE_CONFIGS = new WeakHashMap<>();
+    private static final HashMap<String,Object> LATTE_CONFIGS = new HashMap<>();
     private static final ArrayList<IconFontDescriptor> ICONS = new ArrayList<>();
 
     private Configurator(){
@@ -22,7 +23,7 @@ public class Configurator {
         return Holder.INSTANCE;
     }
 
-    final WeakHashMap<String, Object> getLatteConfigs(){
+    final HashMap<String, Object> getLatteConfigs(){
         return LATTE_CONFIGS;
     }
 
