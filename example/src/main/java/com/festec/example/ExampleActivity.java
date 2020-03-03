f997595535853c9f2727ccde71313a73b2c2ebb7
Tony <tony@gmail.com>
2020-03-03 14:39:40 +0800

    4-2 单Activity界面架构验证

---
+++ example/src/main/java/com/festec/example/ExampleActivity.java
@@ -0,0 +1,18 @@
+package com.festec.example;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import com.example.latte.activities.ProxyActivity;
+import com.example.latte.app.Latte;
+import com.example.latte.delegates.LatteDelegate;
+
+public class ExampleActivity extends ProxyActivity {
+
+
+    @Override
+    public LatteDelegate setRootDelegate() {
+        return new ExampleDelegate();
+    }
+}
---
+++ example/src/main/java/com/festec/example/exampleApp.java
@@ -0,0 +1,25 @@
+package com.festec.example;
+
+
+import android.app.Application;
+
+import com.example.latte.app.Latte;
+import com.example.latte.ec.icon.FontEcModule;
+import com.joanzapata.iconify.fonts.FontAwesomeModule;
+
+/**
+ * Created by 25400 on 2020/2/26.
+ */
+
+public class exampleApp extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Latte.init(this)
+                .withIcon(new FontAwesomeModule())
+                .withIcon(new FontEcModule())
+                .withApiHost("http://127.0.0.1/")
+                .configure();
+    }
+}
