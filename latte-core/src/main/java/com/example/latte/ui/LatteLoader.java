e1b4374b55f7f88ce87e9c47d786d653da6e498f
Tony <tony@gmail.com>
2020-03-10 13:22:44 +0800

    5-5 Loading框架集成与完善(AVLoadingIndicatorView)-1

---
+++ latte-core/src/main/java/com/example/latte/ui/LatteLoader.java
@@ -0,0 +1,17 @@
+package com.example.latte.ui;
+
+import android.content.Context;
+import android.support.v7.app.AppCompatDialog;
+
+/**
+ * Created by 25400 on 2020/3/10.
+ */
+
+public class LatteLoader {
+
+    public static void showLoading(Context context, String type){
+
+        final AppCompatDialog dialog = new AppCompatDialog(context);
+
+    }
+}
---
+++ latte-core/src/main/java/com/example/latte/ui/LoaderCreator.java
@@ -0,0 +1,50 @@
+package com.example.latte.ui;
+
+import android.content.Context;
+
+import com.wang.avi.AVLoadingIndicatorView;
+import com.wang.avi.Indicator;
+
+import java.util.WeakHashMap;
+
+/**
+ * Created by 25400 on 2020/3/10.
+ */
+
+public final class LoaderCreator {
+
+    private static final WeakHashMap<String,Indicator> LOADING_MAP = new WeakHashMap<>();
+
+    static AVLoadingIndicatorView create(String type, Context context){
+
+        final AVLoadingIndicatorView avLoadingIndicatorView = new AVLoadingIndicatorView(context);
+        if (LOADING_MAP.get(type) == null){
+            final Indicator indicator = getIndicator(type);
+            LOADING_MAP.put(type, indicator);
+        }
+        avLoadingIndicatorView.setIndicator(LOADING_MAP.get(type));
+        return avLoadingIndicatorView;
+    }
+
+    private static Indicator getIndicator(String name){
+        if(name == null || name.isEmpty()){
+            return null;
+        }
+        final StringBuilder drawableClassName = new StringBuilder();
+        if(!name.contains(".")){
+            final String defaultPackageName = AVLoadingIndicatorView.class.getPackage().getName();//反射获取包名
+            drawableClassName.append(defaultPackageName)
+                    .append(".indicators")
+                    .append(".");
+        }
+        drawableClassName.append(name);//生成具体View
+        try {
+            final Class<?> drawableClass = Class.forName(drawableClassName.toString());
+            return (Indicator) drawableClass.newInstance();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+}
