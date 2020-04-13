02cd4debb77d626e42dc835fec439619b72fc905
Tony <tony@gmail.com>
2020-04-13 14:56:39 +0800

    9-2 底部导航BottomBar设计与实现-2

--- latte-core/src/main/java/com/example/latte/delegates/bottom/BaseBottomDelegate.java
+++ latte-core/src/main/java/com/example/latte/delegates/bottom/BaseBottomDelegate.java
@@ -4,18 +4,29 @@ import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.ColorInt;
 import android.support.annotation.Nullable;
+import android.support.v7.widget.AppCompatTextView;
+import android.support.v7.widget.LinearLayoutCompat;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.RelativeLayout;
 
+import com.example.latte.R;
+import com.example.latte.R2;
 import com.example.latte.delegates.LatteDelegate;
+import com.joanzapata.iconify.widget.IconTextView;
 
 import java.util.ArrayList;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
+import butterknife.BindView;
+import me.yokeyword.fragmentation.SupportFragment;
+
 /**
  * Created by 25400 on 2020/4/11.
  */
 
-public abstract class BaseBottomDelegate extends LatteDelegate {
+public abstract class BaseBottomDelegate extends LatteDelegate implements View.OnClickListener{
 
     private final ArrayList<BottomTabBean> TAB_BEANS = new ArrayList<>();
     private final ArrayList<BottomItemDelegate> ITEM_DELEGATES = new ArrayList<>();
@@ -24,7 +35,15 @@ public abstract class BaseBottomDelegate extends LatteDelegate {
     private int mIndexDelegate = 0;
     private int mClickedColor = Color.RED;
 
+    @BindView(R2.id.bottom_bar)
+    LinearLayoutCompat mBottomBar = null;
+
     public abstract LinkedHashMap<BottomTabBean, BottomItemDelegate> setItems(ItemBuilder builder);
+
+    @Override
+    public Object setLayout() {
+        return R.layout.delegate_bottom;
+    }
 
     public abstract int setIndexDelegate();
 
@@ -48,5 +67,55 @@ public abstract class BaseBottomDelegate extends LatteDelegate {
             TAB_BEANS.add(key);
             ITEM_DELEGATES.add(value);
         }
+    }
+
+    @Override
+    public void onBindView(@Nullable Bundle savedInstanceState, View rootView) {
+        final int size = ITEMS.size();
+        for (int i = 0; i < size; i++) {
+            LayoutInflater.from(getContext()).inflate(R.layout.bottom_item_icon_text_layout,mBottomBar);
+            final RelativeLayout item = (RelativeLayout) mBottomBar.getChildAt(i);
+            //设置每个item的点击事件
+            item.setTag(i);
+            item.setOnClickListener(this);
+            final IconTextView itemIcon = (IconTextView) item.getChildAt(0);
+            final AppCompatTextView itemTitle = (AppCompatTextView) item.getChildAt(1);
+            final BottomTabBean bean = TAB_BEANS.get(i);
+            //初始化数据
+            itemIcon.setText(bean.getIcon());
+            itemTitle.setText(bean.getTitle());
+            if (i == mIndexDelegate){
+                itemIcon.setTextColor(mClickedColor);
+                itemTitle.setTextColor(mClickedColor);
+            }
+        }
+
+        final SupportFragment[] delegateArray = ITEM_DELEGATES.toArray(new SupportFragment[size]);
+        loadMultipleRootFragment(R.id.bottom_bar_delegate_container, mIndexDelegate,delegateArray);
+    }
+
+    private void resetColor(){
+        final int count = mBottomBar.getChildCount();
+        for(int i=0;i<count;i++){
+            final RelativeLayout item = (RelativeLayout) mBottomBar.getChildAt(i);
+            final IconTextView itemIcon = (IconTextView) item.getChildAt(0);
+            itemIcon.setTextColor(Color.GRAY);
+            final AppCompatTextView itemTitle = (AppCompatTextView) item.getChildAt(1);
+            itemTitle.setTextColor(Color.GRAY);
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+        final int tag = (int) v.getTag();
+        resetColor();
+        final RelativeLayout item = (RelativeLayout) v;
+        final IconTextView itemIcon = (IconTextView) item.getChildAt(0);
+        itemIcon.setTextColor(mClickedColor);
+        final AppCompatTextView itemTitle = (AppCompatTextView) item.getChildAt(1);
+        itemTitle.setTextColor(mClickedColor);
+        showHideFragment(ITEM_DELEGATES.get(tag),ITEM_DELEGATES.get(mCurrentDelegate));
+        //注意先后顺序
+        mCurrentDelegate = tag;
     }
 }
