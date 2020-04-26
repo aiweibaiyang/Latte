ab6d73abc79cf84eb6d57a3dac35cd13475fb8d5
Tony <tony@gmail.com>
2020-04-26 22:47:03 +0800

    11-2 分类左侧列表数据解析与UI呈现

---
+++ latte-ec/src/main/java/com/example/latte/ec/main/sort/list/SortRecyclerAdapter.java
@@ -0,0 +1,80 @@
+package com.example.latte.ec.main.sort.list;
+
+import android.graphics.Color;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.widget.AppCompatTextView;
+import android.view.View;
+
+import com.example.latte.ec.R;
+import com.example.latte.ec.main.sort.SortDelegate;
+import com.example.latte.ui.recycler.ItemType;
+import com.example.latte.ui.recycler.MultipleFields;
+import com.example.latte.ui.recycler.MultipleItemEntity;
+import com.example.latte.ui.recycler.MultipleRecyclerAdapter;
+import com.example.latte.ui.recycler.MultipleViewHolder;
+
+import java.util.List;
+
+/**
+ * Created by 25400 on 2020/4/24.
+ */
+
+public class SortRecyclerAdapter extends MultipleRecyclerAdapter {
+
+    private final SortDelegate DELEGATE;
+    private int mPrePosition = 0;
+
+    protected SortRecyclerAdapter(List<MultipleItemEntity> data, SortDelegate delegate) {
+        super(data);
+        this.DELEGATE = delegate;
+        //添加垂直菜单布局
+        addItemType(ItemType.VERTICAL_MENU_LIST, R.layout.item_vertical_menu_list);
+    }
+
+    @Override
+    protected void convert(final MultipleViewHolder holder, final MultipleItemEntity entity) {
+        super.convert(holder, entity);
+        switch (holder.getItemViewType()) {
+            case ItemType.VERTICAL_MENU_LIST:
+                final String text = entity.getFiled(MultipleFields.TEXT);
+                final boolean isClicked = entity.getFiled(MultipleFields.TAG);
+                final AppCompatTextView name = holder.getView(R.id.tv_vertical_item_name);
+                final View line = holder.getView(R.id.view_line);
+                final View itemView = holder.itemView;
+                itemView.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        final int currentPosition = holder.getAdapterPosition();
+                        if (mPrePosition != currentPosition){
+                            //还原上一个
+                            getData().get(mPrePosition).setField(MultipleFields.TAG,false);
+                            notifyItemChanged(mPrePosition);
+                            //更新选中的部分
+                            entity.setField(MultipleFields.TAG,true);
+                            notifyItemChanged(currentPosition);
+                            mPrePosition = currentPosition;
+
+                            final int contentId = getData().get(currentPosition).getFiled(MultipleFields.ID);
+                        }
+                    }
+                });
+
+                if (!isClicked) {
+                    line.setVisibility(View.INVISIBLE);
+                    name.setTextColor(ContextCompat.getColor(mContext, R.color.we_chat_black));
+                    itemView.setBackgroundColor(ContextCompat.getColor(mContext, R.color.item_background));
+                } else {
+                    line.setVisibility(View.VISIBLE);
+                    name.setTextColor(ContextCompat.getColor(mContext, R.color.app_main));
+                    line.setBackgroundColor(ContextCompat.getColor(mContext, R.color.app_main));
+                    itemView.setBackgroundColor(Color.WHITE);
+                }
+
+                holder.setText(R.id.tv_vertical_item_name, text);
+
+                break;
+            default:
+                break;
+        }
+    }
+}
--- latte-ec/src/main/java/com/example/latte/ec/main/sort/list/VerticalListDelegate.java
+++ latte-ec/src/main/java/com/example/latte/ec/main/sort/list/VerticalListDelegate.java
@@ -2,23 +2,66 @@ package com.example.latte.ec.main.sort.list;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
 
 import com.example.latte.delegates.LatteDelegate;
 import com.example.latte.ec.R;
+import com.example.latte.ec.R2;
+import com.example.latte.ec.main.sort.SortDelegate;
+import com.example.latte.net.RestClient;
+import com.example.latte.net.callback.ISuccess;
+import com.example.latte.ui.recycler.MultipleItemEntity;
+
+import java.util.List;
+
+import butterknife.BindView;
 
 /**
  * Created by 25400 on 2020/4/23.
  */
 
 public class VerticalListDelegate extends LatteDelegate {
+
+    @BindView(R2.id.rv_vertical_menu_list)
+    RecyclerView mRecyclerView = null;
+
     @Override
     public Object setLayout() {
         return R.layout.delegate_vertical_list;
     }
 
+    private void initRecyclerView(){
+        final LinearLayoutManager manager = new LinearLayoutManager(getContext());
+        mRecyclerView.setLayoutManager(manager);
+        //屏蔽动画效果
+        mRecyclerView.setItemAnimator(null);
+    }
+
     @Override
     public void onBindView(@Nullable Bundle savedInstanceState, View rootView) {
+        initRecyclerView();
+    }
 
+    @Override
+    public void onLazyInitView(@Nullable Bundle savedInstanceState) {
+        super.onLazyInitView(savedInstanceState);
+        RestClient.builder()
+                .url("sort_list.php")
+                .loader(getContext())
+                .success(new ISuccess() {
+                    @Override
+                    public void onSuccess(String response) {
+                        final List<MultipleItemEntity> data =
+                                new VerticalListDataConverter().setJsonData(response).convert();
+                        final SortDelegate delegate = getParentDelegate();
+                        final SortRecyclerAdapter adapter = new SortRecyclerAdapter(data,delegate);
+                        mRecyclerView.setAdapter(adapter);
+
+                    }
+                })
+                .build()
+                .get();
     }
 }
